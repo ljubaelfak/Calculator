538af84e06c5d8f944f3dabe8d2b6ef204882690
ljubaelfak <51276179+ljubaelfak@users.noreply.github.com>
2019-06-05 20:09:48 +0200

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+from math import *
+
+the_result = 0
+Frist_number = float(input())
+Operations = "Operations available: add(+), sub(-), mult(*), div(/), sqrt, pow(**)"
+print(Operations)
+Operation = str(input("Choose the operation: "))
+if Operation == "sqrt":
+    the_result = sqrt(Frist_number)
+else:
+    Second_number = float(input())
+    if Operation == "add":
+        the_result = Frist_number + Second_number
+    elif Operation == "sub":
+        the_result = Frist_number - Second_number
+    elif Operation == "mult":
+        the_result = Frist_number * Second_number
+    elif Operation == "div":
+        the_result = Frist_number / Second_number
+    elif Operation == "pow":
+        the_result = Frist_number ** Second_number
+    else:
+        print("Operation invalid!!!")
+
+print(the_result)
